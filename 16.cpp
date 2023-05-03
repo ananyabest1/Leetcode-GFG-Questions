143d4fbc295384a791c532004bc6296159f73c3a
ananyabest1 <ananyasinghfl21@gmail.com>
2023-05-03 23:13:24 +0530

    Code

--- 16.cpp
+++ 16.cpp
@@ -7,16 +7,16 @@ public:
     int solve(int n) {
         if(n <= 1) return 1;
         if(dp[n]) return dp[n];
-        int ans = 0;
+        int A = 0;
         for(int i=1; i <= n; i++) {
-            ans += solve(i-1) * solve(n-i);
+            A += solve(i-1) * solve(n-i);
         }
-        return dp[n] = ans;
+        return dp[n] = A;
     }
 
     int numTrees(int n) {
         memset(dp, 0, sizeof(dp));
-        int ans = solve(n);
-        return ans;
+        int A = solve(n);
+        return A;
     }
 };
