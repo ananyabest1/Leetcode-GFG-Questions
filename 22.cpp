578c79b8e5c7d55d13d05465d17bd68b48c98ef4
ananyabest1 <ananyasinghfl21@gmail.com>
2023-05-09 22:09:28 +0530

    Code

---
+++ 22.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+     int n=nums.size();
+     vector<int>ans;
+     unordered_map<int,int>mp;
+     for(int i=0; i<n; i++){
+         mp[nums[i]]++;
+         if(mp[nums[i]]>n/3){
+             ans.push_back(nums[i]);
+             mp[nums[i]]=INT_MIN;
+         }
+     }  
+     return ans;
+    }
+};
