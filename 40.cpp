d71aec23e11d5d40923e3b29460f7d2de3a51305
ananyabest1 <ananyasinghfl21@gmail.com>
2023-06-08 19:42:01 +0530

    Code

---
+++ 40.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int j=abs(nums[i])-1;
+            if(nums[j]>0)
+            nums[j]*=-1;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+            ans.push_back(i+1);
+        }
+        return ans;
+    }
+};
+int main(){
+   vector < int > nums = { 1, 1, 0, 1, 1, 1 };
+   Solution obj;
+   int ans = obj.findDisappearedNumbers(nums);
+    cout << "The maximum  consecutive 1's are " << ans;
+    return 0;
+    
+
+}
